feca96a1febec0c171d6ee3f7c9fe0ca2a99ccab
Simon Henrotte <henrotte.simon@gmail.com>
2014-10-29 08:25:35 +0100

    First commit - init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gulp boilerplate
+
+Init project
---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+/////////////////////////////////////////////////////////////////////
+//////////////          Gulp Boilerplate        //////////////
+/////////////////////////////////////////////////////////////////////
+
+//////////////              Variables             //////////////
+
+var env = 'development'; // Available : development, staging, prodution
+
+var input = 'src/';
+var output = 'app/':
+
+var scss = input+'scss/';
+var js = input+'javascript/';
+var img = input+'images/';
+var fonts = input+'fonts/';
+
+//////////////           Dependencies          //////////////
+
+
+
+//////////////                 Tasks               //////////////
+
+gulp.task("default", [])
